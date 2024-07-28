abb55ef0fecda65daeeb0bba3be62cbc3d06f9a8
Ali Sjd <146744363+Ali-Sajedi-Shahgoli@users.noreply.github.com>
2024-07-28 14:05:37 +0330

    Add files via upload

---
+++ STM32F103 -Core Config with RCC Seetup/STM32_Blinker/Core/Inc/main.h
@@ -0,0 +1,21 @@
+/*
+ * main.h
+ *
+ *  Created on: Jul 28, 2024
+ *      Author: Ali
+ */
+
+#ifndef INC_MAIN_H_
+#define INC_MAIN_H_
+
+
+
+#include <stm32f1xx.h>
+#include <stm32f103xb.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+
+
+
+#endif /* INC_MAIN_H_ */
---
+++ STM32F103 -Core Config with RCC Seetup/STM32_Blinker/Core/Src/main.c
@@ -0,0 +1,17 @@
+
+
+
+#include "main.h"
+
+int main(void)
+{
+
+	RCC_HSE_CLOCK();
+	RCC_SYSTICK_CONFIG(72000);
+	while(1)
+	{
+
+
+
+	}
+}
